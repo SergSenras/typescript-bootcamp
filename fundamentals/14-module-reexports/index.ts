2c570a4a04064b6f9a228daf0e76e6dc00a0fda0
SergSenras <SergSenras@gmail.com>
2023-06-14 22:05:28 +0300

    module reexport section

---
+++ fundamentals/14-module-reexports/index.ts
@@ -0,0 +1,9 @@
+import {Course} from "./course-model";
+import {loadAllCourses} from "./feature-1";
+import {saveCourse} from "./feature-2";
+
+export {
+    Course,
+    loadAllCourses,
+    saveCourse
+};
