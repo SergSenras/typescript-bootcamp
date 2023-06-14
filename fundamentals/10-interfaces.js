55821c4554d4f17055cd4c942eea261be2cbffdb
SergSenras <SergSenras@gmail.com>
2023-06-14 17:59:14 +0300

    interface lesson

---
+++ fundamentals/10-interfaces.js
@@ -0,0 +1,9 @@
+var course = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    lessonsCount: 10
+};
+var otherCourse = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects"
+};
---
+++ fundamentals/10-interfaces.ts
@@ -0,0 +1,21 @@
+interface Course {
+    readonly title:string;
+    subtitle:string;
+}
+
+const course: Course = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    lessonsCount: 10
+};
+
+/** Interface can be EXTENDED */
+interface Course {
+    lessonsCount?:number;
+}
+
+const otherCourse: Course = {
+    title: "Typescript Bootcamp v2",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    lessonsCount: 10
+};
